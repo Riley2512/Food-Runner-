8187c83a84c42ff5d002010def7843d87901e44e
Riley2512 <rilgol97@kingshigh.school.nz>
2026-07-23 14:59:40 +1200

    made options script

--- main_menu.gd
+++ main_menu.gd
@@ -4,14 +4,9 @@ extends Control
 @onready var options: Panel = $Options
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
 
 
 func _on_start_pressed() -> void:
@@ -21,7 +16,7 @@ func _on_start_pressed() -> void:
 func _on_settings_pressed() -> void:
 	print("Settings pressed")
 	main_buttons.visible = false
-	options.visible
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
