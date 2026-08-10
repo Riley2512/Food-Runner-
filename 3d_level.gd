0c15d62df27a53dbffe261ddb442df837bf9e6fa
Riley2512 <rilgol97@kingshigh.school.nz>
2026-08-10 14:58:27 +1200

    madde a heath bar

--- 3d_level.gd
+++ 3d_level.gd
@@ -1,1 +1,7 @@
 extends Node3D
+
+@onready var target = $Player
+
+
+func _process(delta):
+	get_tree().call_group("enmey", "target_position", target.global_transform.origin)
--- Enmey2.gd
+++ Enmey2.gd
@@ -1,13 +1,64 @@
 extends CharacterBody3D
 
-@onready var nav
-var speed = 3.5 
+var detect = false
+@onready var nav = $NavigationAgent3D
+var speed = 3.5
 var gravity = 9.8
-func _process(delta):
+var is_dead = false
+
+## HEALTH BAR --
+@onready var progress_bar: ProgressBar = $"../EnmeySpawner/CollisionShape3D/CanvasLayer/ProgressBar"
+@onready var timer = $"../Timer"
+
+func _physics_process(delta):
+	if is_dead:
+		return
+
+	if progress_bar.value <= 0:
+		die()
+		return
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
-		velocity.y -=2
-		var next_location = nav.get_next_path_postiton()
-		var current_location = global_transform.origin
-		var new_velocity = (new_loaction - current_location).normlized()* speed
+		velocity.y = -2
+
+	var next_location = nav.get_next_path_position()
+	var current_location = global_transform.origin
+
+	# Only steer on the horizontal plane so we don't fight gravity
+	# or normalize() a (near) zero vector.
+	var direction = next_location - current_location
+	direction.y = 0
+
+	if direction.length() > 0.01:
+		var new_velocity = direction.normalized() * speed
+		velocity.x = move_toward(velocity.x, new_velocity.x, 0.25)
+		velocity.z = move_toward(velocity.z, new_velocity.z, 0.25)
+	else:
+		velocity.x = move_toward(velocity.x, 0, 0.25)
+		velocity.z = move_toward(velocity.z, 0, 0.25)
+
+	move_and_slide()
+
+func die():
+	if is_dead:
+		return
+	is_dead = true
+	print("dead")
+	set_physics_process(false)
+	queue_free()
+
+func set_target(target):
+	nav.target_position = target
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		timer.start()
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		timer.stop()
+
+func _on_timer_timeout():
+	progress_bar.value -= 10
