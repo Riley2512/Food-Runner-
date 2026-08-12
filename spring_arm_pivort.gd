b46b0da10b35f8929818846617aca950c4be00a6
Riley2512 <rilgol97@kingshigh.school.nz>
2026-08-13 10:58:19 +1200

    making a third person player

--- spring_arm_pivort.gd
+++ spring_arm_pivort.gd
@@ -1,9 +1,10 @@
-extends SpringArm3D
+extends Node3D
 
 @export var mouse_sensibilty: float = 0.005
 @export_range(-90.0, 0.0, 0.1, "radians_as_degrees") var min_vertical_angle: float = -PI/2
 @export_range(-90.0, 0.0, 0.1, "radians_as_degrees") var max_vertical_angle: float = -PI/4
 
+@onready var spring_arm := $SpringArm3D
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -17,13 +18,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotation.x -= event.relative.y * mouse_sensibilty
 		rotation.x = clamp(rotation.x, min_vertical_angle, max_vertical_angle)
 		
-	if event.is_action_pressed("wheel_up"):
-		spring_length -= 1
-	if event.is_action_pressed("wheel_down"):
-		spring_length += 1
-		
 	if event.is_action_pressed("toggle_mouse_capture"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+	if event.is_action_pressed("wheel_up"):
+		$SpringArm3D.spring_length -= 1
+	if event.is_action_pressed("wheel_down"):
+		$SpringArm3D.spring_length += 1
+		
+func _process(_delta : float) -> void:
+	global_position = $ "..".global_position
+	
